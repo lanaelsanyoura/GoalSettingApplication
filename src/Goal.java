2200453217f9a41eb8757cb9d8bb71d15915a2c9
Emily Ayala <emily.ayala@mail.utoronto.ca>
2016-11-19 22:36:10 -0500

    Goal further worked on

--- src/Goal.java
+++ src/Goal.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import com.sun.jmx.snmp.Timestamp;
 
 public class Goal {
-	ArrayList<MileStone> mileStones = new ArrayList<MileStone>();
-	ArrayList<CheckIns> checkIns = new ArrayList<CheckIns>();
+	LinkedList<MileStone> mileStones = new LinkedList<MileStone>();
+	ArrayList<GoalCheckIn> checkIns = new ArrayList<GoalCheckIn>();
 	String description;
 	Timestamp start;
 	MileStone nextCheckPoint;
 	Timestamp end;
 	double progress;
 	
-	public Goal(Arraylist<MileStone> mileStones, ArrayList<CheckIns> checkIns, String description, Timestamp end, Timestamp start, double progress){
+	public Goal(LinkedList<MileStone> mileStones, ArrayList<GoalCheckIn> checkIns, String description, Timestamp end, Timestamp start, double progress){
 		this.mileStones = mileStones;
 		this.checkIns = checkIns;
 		this.description = description;
@@ -22,5 +23,16 @@ public class Goal {
 			this.nextCheckPoint = mileStones.get(0);
 		}
 	}
-	
+	public void addMileStone(int indexOfNewMileStone, MileStone newMileStone){
+		if(this.mileStones.get(indexOfNewMileStone) == nextCheckPoint){
+			nextCheckPoint = newMileStone;
+		}
+		this.mileStones.add(indexOfNewMileStone, newMileStone);
+	}
+	public void removeMileStone(int indexToRemove){
+		if(this.mileStones.get(indexToRemove) == nextCheckPoint){
+			nextCheckPoint = this.mileStones.get(indexToRemove + 1);
+		}
+		this.mileStones.remove(indexToRemove);
+	}
 }
