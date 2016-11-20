da9539ba41274bc717c07037ade52881622630ed
Emily Ayala <emily.ayala@mail.utoronto.ca>
2016-11-19 22:15:28 -0500

    MileStone added, constructor for Goal implemented

--- src/Goal.java
+++ src/Goal.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+
+import com.sun.jmx.snmp.Timestamp;
 
 public class Goal {
-
+	ArrayList<MileStone> mileStones = new ArrayList<MileStone>();
+	ArrayList<CheckIns> checkIns = new ArrayList<CheckIns>();
+	String description;
+	Timestamp start;
+	MileStone nextCheckPoint;
+	Timestamp end;
+	double progress;
+	
+	public Goal(Arraylist<MileStone> mileStones, ArrayList<CheckIns> checkIns, String description, Timestamp end, Timestamp start, double progress){
+		this.mileStones = mileStones;
+		this.checkIns = checkIns;
+		this.description = description;
+		this.start = start;
+		this.end = end;
+		this.progress = 0;
+		if(mileStones.size() > 0){
+			this.nextCheckPoint = mileStones.get(0);
+		}
+	}
+	
 }
