0a1227eb36ab6c589969ba3e225639f4653b1526
harrig12 <cait.harrigan@mail.utoronto.ca>
2016-11-20 01:06:02 -0500

    Check in structure updated

---
+++ src/GoalCheckIn.java
@@ -0,0 +1,45 @@
+public class GoalCheckIn {
+	private Goal goal;
+	//percent progress made on the next milestone 
+	private Integer progress;
+	//hours spent on goal since last checkin
+	private Integer hrs;
+	
+	public Goal getGoal() {
+		return goal;
+	}
+
+	public void setGoal(Goal goal) {
+		this.goal = goal;
+	}
+
+	public Integer getProgress() {
+		return progress;
+	}
+
+	public void setProgress(Integer progress) {
+		this.progress = progress;
+	}
+
+	public Integer getHrs() {
+		return hrs;
+	}
+
+	public void setHrs(Integer hrs) {
+		this.hrs = hrs;
+	}
+
+	public GoalCheckIn(Goal goal, Integer progress, Integer hours){
+		this.goal = goal;
+		this.progress = progress;
+		this.hrs = hours;
+	}
+	
+	public String toString(){
+		Double progPerHour = (double) (this.progress/this.hrs);
+		StringBuilder r = new StringBuilder();
+		r.append("On " + this.goal.getName() + ", you made: " + progPerHour.toString() + "% progress per hour");
+	}
+}
+
+  
